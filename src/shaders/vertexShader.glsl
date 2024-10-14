004dfb250603534884778a050f667c6698908c39
Seth H <holtzmans@merrimack.edu>
2024-10-13 23:30:16 -0400

    Feat: Added transformation matrixs to vertex Shader

--- src/shaders/vertexShader.glsl
+++ src/shaders/vertexShader.glsl
@@ -1,6 +1,10 @@
 out float rand;
-out vec4 transformPos; 
+out vec4 finalPos; 
+
 uniform float scaleFactor;
+uniform float time;
+uniform vec3 tVec;
+uniform vec3 rVec;
 
 // Used to get random numbers
 // Found here https://stackoverflow.com/questions/5149544/can-i-generate-a-random-number-inside-a-pixel-shader
@@ -14,21 +18,62 @@ float random( vec2 p )
     return fract( cos( dot(p,K1) ) * 12345.6789 );
 }
 
-void main() 
+void main()
 {  
     // Grab a random number based on the position of the vertex
     rand = random( vec2( position.x, position.y ) );
 
     //scaling matrix 
     mat4 scalingMat = mat4(
-        scaleFactor, 0.0, 0.0, 0.0,
+        scaleFactor , 0.0, 0.0, 0.0,
         0.0, scaleFactor, 0.0, 0.0,
         0.0, 0.0, scaleFactor, 0.0,
         0.0, 0.0,      0.0 , 1
     );
+    
+    //rotation matrixes
+    float theta;
+    //theta for x
+    theta = rVec.x * time * 0.2;
+    mat4 rotateXMat = mat4(
+        1.0, 0.0,               0.0,               0.0,
+        0.0, cos(theta), -sin(theta), 0.0,
+        0.0, sin(theta), cos(theta),0.0,
+        0.0, 0.0,            0.0,              1.0
 
+    );
+
+    //theta for y
+    theta = rVec.y * time * 0.2;
+    mat4 rotateYMat = mat4(
+        cos(theta), 0.0, -sin(theta),  0.0,
+        0.0, 1.0, 0.0, 0.0,
+        sin(theta), 0.0,  cos(theta), 0.0,
+        0.0,0.0, 0.0, 1.0
+    );
+
+    //theta for z 
+    theta = rVec.z * time * 0.2;
+    mat4 rotateZMat = mat4(
+        cos(theta), -sin(theta), 0.0, 0,
+        sin(theta), cos(theta), 0, 0,
+        0, 0, 1, 0,
+        0, 0, 0, 1
+    );
+    
+    //float t = 2.0;
+    //translation mat not working - it displeases the shader gods 
+    mat4 translateMat = mat4(
+         1.0, 0.0, 0.0, 0.0,
+         0.0, 1.0, 0.0, 0.0,
+         0.0, 0.0, 1.0, 0.0,
+         0.0, 0.0, 0.0, 1.0
+    );
+
+    vec4 transformVec = rotateZMat * rotateYMat * rotateXMat * scalingMat *  vec4(position, 1.0);
     //scale our position by our scaling factor
-    transformPos = scalingMat * vec4(position, 1.0);
+    finalPos = projectionMatrix * viewMatrix * modelMatrix * transformVec;
 
-    gl_Position = projectionMatrix * viewMatrix * modelMatrix * transformPos;
+    //gl_Position = projectionMatrix * viewMatrix * modelMatrix * transformPos;
+    gl_Position =  finalPos;
 }
