2fe24004941cfe72e598b1b87fd9568d28d9e3cf
SethMC26 <holtzmans@merrimack.edu>
2024-10-18 18:06:13 -0400

    Refactor: All asteroids now scale at once(I like this better)

--- src/spaceObjects/Asteroid.js
+++ src/spaceObjects/Asteroid.js
@@ -45,8 +45,8 @@ export default class Asteroid {
 
         //speed each asteroid scales up and down 
         //not sure if i like each asteroid scaling up and down by itself or all asteroids scaling together better
-        this.scaleSpeed = THREE.MathUtils.randFloat(.0001,2)
-        //this.scaleSpeed = 1.5;
+        //this.scaleSpeed = THREE.MathUtils.randFloat(.0001,2)
+        this.scaleSpeed = 1.35;
         
         //set random size of asteroid 
         let size = THREE.MathUtils.randFloat(15, 20);
